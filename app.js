d7a5c928ac7278e573200386f3e08b785e5d7584
ywf154 <ywf154@qq.com>
2023-12-12 12:49:03 +0200

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+const app = require('./app');
+
+
+
+app.listen(8082, (req,res) => {
+  console.log(`Listening on port ${port}/n ${process.env.DB_URL}`);
+});
+
+
